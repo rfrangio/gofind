05a45c6abb861ddbdba989e29b206b077400a758
rbf <robertfrangioso@gmail.com>
2015-02-27 18:57:24 -0500

    added output func to read msg's from channel and display

--- gofind.go
+++ gofind.go
@@ -12,25 +12,62 @@ import (
 	"bytes"
 )
 
-func find(root string, wg *sync.WaitGroup, exp []string) error {
+type msg_type int
+
+const (
+	OUTPUT msg_type = 1 + iota
+	ERROR
+	CLOSE
+) 
+
+type output_msg struct {
+	mtype	msg_type
+	buffer	bytes.Buffer
+}
+
+func find(root string, wg *sync.WaitGroup, exp []string, output chan output_msg) error {
 
 	defer wg.Done()
 	var cmd_out bytes.Buffer
+	var msg output_msg 
 
 	findargs := append([]string{root}, exp... )
 	cmd := exec.Command("find", findargs... )
 	cmd.Stdout = &cmd_out
 	err := cmd.Run()
 
-	if(cmd_out.Len() > 0) {
-		fmt.Printf("%s", cmd_out.String())
-	}
+	msg.mtype = OUTPUT
+	msg.buffer = cmd_out
+	output <- msg
+
 	return err
+}
+
+func aggregate(wg *sync.WaitGroup, input chan output_msg) {
+
+	defer wg.Done()
+	var msg output_msg
+
+	for true {
+		msg = <- input
+		switch msg.mtype {
+		case CLOSE:
+			return
+		case OUTPUT:
+			if msg.buffer.Len() > 0 {
+				fmt.Printf("%s", msg.buffer.String())
+			}	
+		case ERROR:
+		default:
+		}
+	}
+
 }
 
 func main() {
 
-	var wg sync.WaitGroup
+	var wg, wga sync.WaitGroup
+	msg_channel := make(chan output_msg)
 
 	flag.Parse()
 	root := flag.Arg(0)
@@ -46,15 +83,20 @@ func main() {
 
 	shallowfind := append(append([]string{},[]string{"-maxdepth", "1"}... ), argslice[1:]... )
 	wg.Add(1)
-	go find(root, &wg, shallowfind) 
+	go find(root, &wg, shallowfind, msg_channel) 
 
 	for  dir := range basedirs {
 		if basedirs[dir].IsDir() {
 			wg.Add(1)
-			go find(filepath.Join(root, basedirs[dir].Name()), &wg, argslice[1:])
+			go find(filepath.Join(root, basedirs[dir].Name()), &wg, argslice[1:], msg_channel)
 		}
 	}
 
+	wga.Add(1)
+	go aggregate(&wga, msg_channel)
+
 	wg.Wait()
+	msg_channel <- output_msg{CLOSE, bytes.Buffer{}}
+	wga.Wait()
 }
 
